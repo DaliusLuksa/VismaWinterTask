48b6271759351adea133251acff23a0fd1661e06
dalius.luksa1 <dalius.luksa1@gmail.com>
2021-01-07 13:38:23 +0200

    Renamed Form1.cs to Stock.cs

---
+++ Stock.Designer.cs
@@ -0,0 +1,244 @@
+﻿using System.Windows.Forms;
+
+namespace VismaWinterTask
+{
+    partial class Stock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.stockIdBox = new System.Windows.Forms.TextBox();
+            this.stockNameBox = new System.Windows.Forms.TextBox();
+            this.stockCountBox = new System.Windows.Forms.TextBox();
+            this.stockUnitBox = new System.Windows.Forms.TextBox();
+            this.stockSizeBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(556, 231);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "";
+            this.button2.Location = new System.Drawing.Point(302, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "New stock";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(302, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // stockIdBox
+            // 
+            this.stockIdBox.Location = new System.Drawing.Point(91, 275);
+            this.stockIdBox.Name = "stockIdBox";
+            this.stockIdBox.Size = new System.Drawing.Size(100, 20);
+            this.stockIdBox.TabIndex = 4;
+            // 
+            // stockNameBox
+            // 
+            this.stockNameBox.Location = new System.Drawing.Point(91, 301);
+            this.stockNameBox.Name = "stockNameBox";
+            this.stockNameBox.Size = new System.Drawing.Size(100, 20);
+            this.stockNameBox.TabIndex = 5;
+            // 
+            // stockCountBox
+            // 
+            this.stockCountBox.Location = new System.Drawing.Point(91, 327);
+            this.stockCountBox.Name = "stockCountBox";
+            this.stockCountBox.Size = new System.Drawing.Size(100, 20);
+            this.stockCountBox.TabIndex = 6;
+            // 
+            // stockUnitBox
+            // 
+            this.stockUnitBox.Location = new System.Drawing.Point(91, 353);
+            this.stockUnitBox.Name = "stockUnitBox";
+            this.stockUnitBox.Size = new System.Drawing.Size(100, 20);
+            this.stockUnitBox.TabIndex = 7;
+            // 
+            // stockSizeBox
+            // 
+            this.stockSizeBox.Location = new System.Drawing.Point(91, 379);
+            this.stockSizeBox.Name = "stockSizeBox";
+            this.stockSizeBox.Size = new System.Drawing.Size(100, 20);
+            this.stockSizeBox.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(646, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Menu window";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(646, 81);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Orders window";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Unit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 382);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Size";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.stockSizeBox);
+            this.Controls.Add(this.stockUnitBox);
+            this.Controls.Add(this.stockCountBox);
+            this.Controls.Add(this.stockNameBox);
+            this.Controls.Add(this.stockIdBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Stock items";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox stockIdBox;
+        private System.Windows.Forms.TextBox stockNameBox;
+        private System.Windows.Forms.TextBox stockCountBox;
+        private System.Windows.Forms.TextBox stockUnitBox;
+        private System.Windows.Forms.TextBox stockSizeBox;
+        private System.Windows.Forms.Button button1;
+        private Button button4;
+        private Button button5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
+
---
+++ Stock.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VismaWinterTask
+{
+    public partial class Stock : Form
+    {
+        private const string _fileName = "stock.csv";
+        private CSVReader _csvReader;
+
+        public Stock()
+        {
+            InitializeComponent();
+            _csvReader = new CSVReader(@_fileName);
+            // load data on the init
+            DisplayData();
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            stockIdBox.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            stockNameBox.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            stockCountBox.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            stockUnitBox.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            stockSizeBox.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+        }
+
+        // insert button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] param = { stockIdBox.Text, stockNameBox.Text, stockCountBox.Text, stockUnitBox.Text, stockSizeBox.Text };
+            if (_csvReader.InsertItem(param))
+            {
+                // item was successfully added
+                // refresh table content
+                // and clear input fields
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                // item was not added
+                // show error message
+                MessageBox.Show("Item was not added. Please fill all the boxes.");
+            }
+        }
+
+        // update button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (stockIdBox.Text != "" && stockNameBox.Text != "" && stockCountBox.Text != "" && stockUnitBox.Text != "" && stockSizeBox.Text != "")
+            {
+                string updatedItem = $"{stockIdBox.Text},{stockNameBox.Text},{stockCountBox.Text},{stockUnitBox.Text},{stockSizeBox.Text}";
+                if (_csvReader.UpdateItem(int.Parse(stockIdBox.Text), updatedItem))
+                {
+                    ClearData();
+                    DisplayData();
+                }
+                else
+                {
+                    MessageBox.Show("Stock item was not updated.");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Please selet stock item to update and fill all boxes.");
+            }
+        }
+
+        // delete button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (stockIdBox.Text != "")
+            {
+                if (_csvReader.DeleteItem(int.Parse(stockIdBox.Text)))
+                {
+                    ClearData();
+                    DisplayData();
+                }
+                else
+                {
+                    MessageBox.Show("Stock item was not deleted.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select stock item to delete.");
+            }
+        }
+
+        // menu window button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Menu menu = new Menu();
+            menu.Show();
+        }
+
+        // orders window button
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Orders orders = new Orders();
+            orders.Show();
+        }
+
+        // display data in DataGridView
+        private void DisplayData()
+        {
+            dataGridView1.DataSource = _csvReader.ReadFile();
+        }
+
+        // clear Data  
+        private void ClearData()
+        {
+            stockIdBox.Text = "";
+            stockNameBox.Text = "";
+            stockCountBox.Text = "";
+            stockUnitBox.Text = "";
+            stockSizeBox.Text = "";
+        }
+    }
+}
